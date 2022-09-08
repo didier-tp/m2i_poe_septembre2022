f3afae25db9c5ae54f823e0e7a491ae3157dcc19
DidierDefrance <didier@d-defrance.fr>
2022-09-08 14:01:44 +0200

    s

--- tpJava/src/main/java/com/m2i/tp/Personne.java
+++ tpJava/src/main/java/com/m2i/tp/Personne.java
@@ -2,14 +2,18 @@ package com.m2i.tp;
 
 public class Personne {
 	
-	//prenom
-	//nom
+	public String prenom;
+	public String nom;
 	public int age;
 	
 	public void afficher() {
-		System.out.println();
+		System.out.println("Je suis une personne, prenom=" + prenom + " nom=" +nom + " age=" + age );
 	}
 	
-	//... incrementerAge()
+	public void incrementerAge() {
+		this.age = this.age + 1;
+		//age = age+1;
+		//age++;
+	}
 
 }
