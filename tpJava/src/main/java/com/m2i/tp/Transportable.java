d0216a5c0da20acf6cac0c6fc365252f2155ada3
DidierDefrance <didier@d-defrance.fr>
2022-09-09 16:02:03 +0200

    interface Transportable

---
+++ tpJava/src/main/java/com/m2i/tp/Transportable.java
@@ -0,0 +1,15 @@
+package com.m2i.tp;
+
+public interface Transportable {
+     //double getPoids(); //en kg
+	 //double getVolume(); //en litre
+}
+
+/*
+NB: cette interface sera implementée par:
+     Personne
+     Bagage (nouvelle classe)
+     
+et dans la classe Avion , il y aura une soute 
+avec un tableau de choses transportable (bagages , ....)     
+*/
