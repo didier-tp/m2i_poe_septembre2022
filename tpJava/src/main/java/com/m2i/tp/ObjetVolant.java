0c128d296c284cd4f9d63e423776f800fb76e9e3
DidierDefrance <didier@d-defrance.fr>
2022-09-09 15:10:07 +0200

    ObjectVolant abstract

---
+++ tpJava/src/main/java/com/m2i/tp/ObjetVolant.java
@@ -0,0 +1,21 @@
+package com.m2i.tp;
+
+public abstract class ObjetVolant {
+	
+	private int altitudeMax = 500; //par defaut
+	
+	public abstract void decrire();
+
+	public ObjetVolant() {
+	}
+
+	public int getAltitudeMax() {
+		return altitudeMax;
+	}
+
+	public void setAltitudeMax(int altitudeMax) {
+		this.altitudeMax = altitudeMax;
+	}
+	
+
+}
