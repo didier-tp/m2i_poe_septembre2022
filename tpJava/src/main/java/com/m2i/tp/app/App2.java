7832515b66261cc911bb99edf71d0ff0471c4e60
DidierDefrance <didier@d-defrance.fr>
2022-09-12 16:01:30 +0200

    s

--- tpJava/src/main/java/com/m2i/tp/app/App2.java
+++ tpJava/src/main/java/com/m2i/tp/app/App2.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import com.m2i.tp.Personne;
+
 public class App2 {
 
 	public static void main(String[] args) {
@@ -17,6 +19,24 @@ public class App2 {
 		System.out.println("os.name="+System.getProperty("os.name"));
 		//testAncienneCollection();
 		testCollectionModerne();
+		testManipCollection();
+	}
+	
+	public static void testManipCollection() {
+		List<Personne> listePersonnes = new ArrayList<>();
+		listePersonnes.add(new Personne("jean","Bon",31));
+		listePersonnes.add(new Personne("alain","Therieur",25));
+		listePersonnes.add(new Personne("dupond","Durand",45));
+		listePersonnes.add(new Personne("alex","Toto",23));
+		listePersonnes.add(new Personne("sophie","Zorro",53));
+		
+		//etape 1 (effectuer un filtrage)
+		//on va creer une autre collection
+		//qui va comporter que les personnes qui au 40ans au plus:
+		List<Personne> listePersonnes40ansAuplus = new ArrayList<>();
+		
+		
+		//affiche listePersonnes40ansAuplus via le for() au sens forEach
 	}
 	
 	public static void testCollectionModerne() {
