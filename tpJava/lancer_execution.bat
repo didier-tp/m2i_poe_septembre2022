e8144003e0f02259584ac810669d40c884fd9c9a
DidierDefrance <didier@d-defrance.fr>
2022-09-12 14:27:47 +0200

    s

--- tpJava/lancer_execution.bat
+++ tpJava/lancer_execution.bat
@@ -2,9 +2,10 @@ set JAVA_HOME=C:\Program Files\Java\jdk-11.0.12
 set PATH=%JAVA_HOME%\bin;%PATH%
 REM java -version
 
-REM set CLASSPATH=target\tpJava.jar
+set CLASSPATH=target\tpJava.jar
 REM java com.m2i.tp.App
+java -Daff=majuscule com.m2i.tp.App2
 
-java -jar target\tpJava.jar
+REM java -jar target\tpJava.jar
 
 pause
