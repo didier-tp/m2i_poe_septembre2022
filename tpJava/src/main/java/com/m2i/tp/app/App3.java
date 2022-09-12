18021f86e0a9c595d2bf51a46168b3b204082aa8
DidierDefrance <didier@d-defrance.fr>
2022-09-12 18:42:48 +0200

    s

---
+++ tpJava/src/main/java/com/m2i/tp/app/App3.java
@@ -0,0 +1,24 @@
+package com.m2i.tp.app;
+
+public class App3 {
+
+	public static void main(String[] args) {
+		// mini_tp_validation_bases_java
+
+		//1 coder (dans com.m2i.tp) une nouvelle classe "Produit"
+		// comportant un attribut id de type Integer
+		//                        label de type String
+		//                        prix de type Double
+		// avec get/set , contructeurs et méthode .toString()
+		
+		//2 au sein de cette méthode main():
+		// créer une liste de produits (ArrayList<Produit>)
+		
+		//ajouter 4 produits (avec id,label et prix)
+		//calculer la moyenne des prix des produits
+		//par exemple via boucle for , calcul de total et division par nombre d'éléments de la liste
+		
+		//afficher le prix moyen calculé et la liste de tous les produits
+	}
+
+}
