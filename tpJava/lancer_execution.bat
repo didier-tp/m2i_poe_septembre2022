696d0163743658a8832cb36c2d45022c606881c1
DidierDefrance <didier@d-defrance.fr>
2022-09-12 14:39:27 +0200

    propriete systeme

--- tpJava/lancer_execution.bat
+++ tpJava/lancer_execution.bat
@@ -3,8 +3,8 @@ set PATH=%JAVA_HOME%\bin;%PATH%
 REM java -version
 
 set CLASSPATH=target\tpJava.jar
-REM java com.m2i.tp.App
-java -Daff=majuscule com.m2i.tp.App2
+REM java com.m2i.tp.map.App
+java -Daff=majuscule com.m2i.tp.app.App2
 
 REM java -jar target\tpJava.jar
 
--- tpJava/src/main/java/com/m2i/tp/app/App2.java
+++ tpJava/src/main/java/com/m2i/tp/app/App2.java
@@ -3,8 +3,13 @@ package com.m2i.tp.app;
 public class App2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String typeAff = System.getProperty("aff"); //pour récupérer la valeur choisie
+		//au démarrage -Daff=majuscule ou -Daff=minuscule ou ...
+		String username = "Didier Devloppeur Fou";
+		if(typeAff!=null && typeAff.equals("majuscule")) {
+			username =username.toUpperCase();
+		}
+		System.out.println("username="+username);
 	}
 
 }
