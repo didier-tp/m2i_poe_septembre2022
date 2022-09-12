c2c425b916742b6f3af4a1a1bd450578ea2e269d
DidierDefrance <didier@d-defrance.fr>
2022-09-12 11:18:36 +0200

    s

---
+++ tpJava/src/main/java/com/m2i/tp/calcul/Calculs.java
@@ -0,0 +1,14 @@
+package com.m2i.tp.calcul;
+
+public class Calculs {
+
+	
+	public static int division(int a, int b) {
+		return a/b;
+	}
+	
+	public static double racineCarre(double x) {
+		return Math.sqrt(x);
+	}
+
+}
