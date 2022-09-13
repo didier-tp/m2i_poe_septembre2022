9e1eca60f14d3193cbba184c451c932aa3e4e2e4
DidierDefrance <didier@d-defrance.fr>
2022-09-13 16:56:26 +0200

    a

--- tpJava/src/main/java/com/m2i/tp/app/App5.java
+++ tpJava/src/main/java/com/m2i/tp/app/App5.java
@@ -1,6 +1,8 @@
 package com.m2i.tp.app;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Random;
 
@@ -12,6 +14,7 @@ public class App5 {
         int nombreAleatoire = (int) (valeurAleatoire * 100);//entre 0 et 100
         System.out.println("nombreAleatoire="+nombreAleatoire);
         
+        //classes Date , Calendar , SimpleDateFormat qui existent depuis très longtemps (début 2000)
         
         Date dateAujourdhui = new Date();
         System.out.println("dateAujourdhui="+dateAujourdhui);
@@ -25,6 +28,19 @@ public class App5 {
         SimpleDateFormat simpleDateFormatFr = new SimpleDateFormat("dd/MM/yyyy");
         String sdateFr = simpleDateFormatFr.format(dateAujourdhui);
         System.out.println("dateAujourdhui au format francais = sdateFr = " +sdateFr);
+        
+        SimpleDateFormat simpleDateFormatAvecHeure = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        String sdateAvecHeure = simpleDateFormatAvecHeure.format(dateAujourdhui);
+        System.out.println("dateAujourdhui avec heure = " +sdateAvecHeure);
+        
+        Calendar cal = Calendar.getInstance(); 
+        cal.set(2022, 12-1, 25); //noel 2022 (25/12/2022) 
+        Date dateNoel2022 = cal.getTime();
+        System.out.println("noel2022 en francais = " + simpleDateFormatFr.format(dateNoel2022));
+        
+        // avec classes nouvelles de java 8 (java.time.LocalDateTime) 
+        LocalDateTime nowDateTime = LocalDateTime.now();
+        System.out.println("nowDateTime="+nowDateTime);
         
 	}
 
