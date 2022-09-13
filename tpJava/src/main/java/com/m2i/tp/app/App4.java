581789e99d5477515dbaf7f232748f93b5393eb4
DidierDefrance <didier@d-defrance.fr>
2022-09-13 13:43:47 +0200

    s

--- tpJava/src/main/java/com/m2i/tp/app/App4.java
+++ tpJava/src/main/java/com/m2i/tp/app/App4.java
@@ -4,6 +4,10 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.m2i.tp.Personne;
 
 public class App4 {
 
@@ -20,11 +24,25 @@ public class App4 {
 					new BufferedReader(new InputStreamReader(fluxLecture));
 			
 			String ligne;
+			int compteur=0;
+			List<Personne> listePersonnes = new ArrayList<>();
 			do {
+				compteur++;
 				ligne=fluxLectureAvecBuffer.readLine();
-				System.out.println("ligne="+ligne);
+				//System.out.println("ligne="+ligne);
+				if(compteur>1 && ligne != null) {
+					//ligne de données (pas la première avec les significations des colonnes)
+					String[] parties  = ligne.split(";");
+					listePersonnes.add(new Personne(
+							Integer.parseInt(parties[0]),
+							parties[1],
+							parties[2],
+							Integer.parseInt(parties[3])
+							));
+				}
 			}while(ligne!=null);
 			fluxLectureAvecBuffer.close();
+			System.out.println("listePersonnes="+listePersonnes);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
