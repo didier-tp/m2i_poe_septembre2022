6ae74a06b6f9105716170e3cace2c1dc4e5a51be
DidierDefrance <didier@d-defrance.fr>
2022-09-14 15:02:13 +0200

    sql

---
+++ tpJava/init-db.sql
@@ -0,0 +1,18 @@
+
+DROP TABLE IF EXISTS personne;
+
+
+CREATE TABLE personne(
+	numero INTEGER auto_increment,
+	prenom VARCHAR(64),
+	nom VARCHAR(64),
+	age INTEGER,
+	PRIMARY KEY(numero));
+	
+
+INSERT INTO personne (numero,prenom,nom,age) VALUES (1,'alain', 'Therieur' , 35);
+
+INSERT INTO personne (numero,prenom,nom,age) VALUES (2,'axelle', 'Aire' , 25 );
+
+
+SELECT * FROM personne;
--- tpJava/lancerConsoleH2.bat
+++ tpJava/lancerConsoleH2.bat
@@ -1,3 +1,5 @@
 REM set JAVA_HOME=C:\Program Files\Java\jdk-11.0.12
 set H2_PATH=C:\Users\d2fde\.m2\repository\com\h2database\h2\2.1.214
+REM set H2_PATH=C:\Users\Administrateur\.m2\repository\com\h2database\h2\2.1.214
 java -jar %H2_PATH%/h2-2.1.214.jar
+REM jdbc:h2:~/mydb14 ou jdbc:h2:~/mydb à la place jdbc:h2:~/test
