891c12477a0ee421afe8fbfa002bed4a8dd0c05e
DidierDefrance <didier@d-defrance.fr>
2022-09-14 16:19:49 +0200

    s

--- tpJava/src/main/java/com/m2i/tp/app/App6.java
+++ tpJava/src/main/java/com/m2i/tp/app/App6.java
@@ -1,15 +1,19 @@
 package com.m2i.tp.app;
 
-import java.sql.Connection;
+import java.util.List;
 
+import com.m2i.tp.Personne;
 import com.m2i.tp.dao.DaoPersonneJdbc;
 
 public class App6 {
 
 	public static void main(String[] args) {
 		DaoPersonneJdbc daoPersonne  = new DaoPersonneJdbc();
-        Connection cn = daoPersonne.etablirConnection();
-        System.out.println("connection jdbc = " + cn);
+        //Connection cn = daoPersonne.etablirConnection();
+        //System.out.println("connection jdbc = " + cn);
+		
+		List<Personne> listePers = daoPersonne.findAllPersonnes();
+		listePers.stream().forEach(p -> System.out.println(p));
 	}
 
 }
