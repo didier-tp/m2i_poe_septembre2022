02076adabccd6e1d9688b32e69153cf56c81e739
DidierDefrance <didier@d-defrance.fr>
2022-11-04 16:37:01 +0100

    s

--- appliSpringSansSpringBoot2/src/main/java/tp/appliSpring/core/dao/DaoCompteJpa.java
+++ appliSpringSansSpringBoot2/src/main/java/tp/appliSpring/core/dao/DaoCompteJpa.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import tp.appliSpring.core.entity.Compte;
 
@@ -22,18 +23,19 @@ public class DaoCompteJpa implements DaoCompte {
 	}
 
 	@Override
+	@Transactional
 	public Compte save(Compte compte) {
 		try {
-			entityManager.getTransaction().begin();
+			//entityManager.getTransaction().begin();
 			if(compte.getNumero()==null) {
 			    entityManager.persist(compte); //INSERT INTO avec auto_incrementation
 			}
 			else {
 			   entityManager.merge(compte); //UPDATE
 			}
-			entityManager.getTransaction().commit();
+			//entityManager.getTransaction().commit();
 		} catch (Exception e) {
-			entityManager.getTransaction().rollback();
+			//entityManager.getTransaction().rollback();
 			e.printStackTrace();
 		}
 		return compte; //avec numero pas null (quelquefois auto_incrémenté)
--- appliSpringSansSpringBoot2/src/test/java/tp/appliSpring/core/service/TestServiceCompte.java
+++ appliSpringSansSpringBoot2/src/test/java/tp/appliSpring/core/service/TestServiceCompte.java
@@ -29,9 +29,14 @@ public class TestServiceCompte {
 	
 	@Test
 	public void testRechercherCompte() {
-		Compte compte1 = serviceCompte.rechercherCompteParNumero(1L);
-		System.out.println("compte1="+compte1);
-		Assertions.assertTrue(compte1.getNumero()==1L);
+		Compte compte1 = new Compte(null,"CompteXy",200.0);
+		serviceCompte.sauvegarderCompte(compte1);
+		Long numCompte1 = compte1.getNumero(); //numero auto incrémenté
+		System.out.println("numCompte1="+numCompte1);
+		
+		Compte compte1Relu = serviceCompte.rechercherCompteParNumero(numCompte1);
+		System.out.println("compte1Relu="+compte1Relu);
+		Assertions.assertTrue(compte1Relu.getNumero()==1L);
 	}
 
 }
