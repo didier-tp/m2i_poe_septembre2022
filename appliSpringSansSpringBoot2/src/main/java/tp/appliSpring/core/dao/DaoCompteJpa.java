a702823713b91a63a07178433ba7c9700e0a7880
DidierDefrance <didier@d-defrance.fr>
2022-11-07 09:34:26 +0100

    s

--- appliSpringSansSpringBoot2/src/main/java/tp/appliSpring/core/dao/DaoCompteJpa.java
+++ appliSpringSansSpringBoot2/src/main/java/tp/appliSpring/core/dao/DaoCompteJpa.java
@@ -16,30 +16,56 @@ public class DaoCompteJpa implements DaoCompte {
 	@PersistenceContext //pour initialiser entityManager à partir du fichier META-INF/persistence.xml
 	                    //ou bien depuis une config équivalente de spring
 	private EntityManager entityManager;
+	
+	/*
+	@Autowired
+	EntityManagerFactory entityManagerFactory;
+	*/
 
 	@Override
 	public Compte findById(Long numCpt) {
 		return entityManager.find(Compte.class, numCpt);
 	}
-
+	
+	
 	@Override
 	@Transactional
 	public Compte save(Compte compte) {
 		try {
-			//entityManager.getTransaction().begin();
 			if(compte.getNumero()==null) {
 			    entityManager.persist(compte); //INSERT INTO avec auto_incrementation
 			}
 			else {
 			   entityManager.merge(compte); //UPDATE
 			}
-			//entityManager.getTransaction().commit();
 		} catch (Exception e) {
-			//entityManager.getTransaction().rollback();
 			e.printStackTrace();
 		}
 		return compte; //avec numero pas null (quelquefois auto_incrémenté)
 	}
+
+
+	/*
+	@Override
+	public Compte save(Compte compte) {
+		EntityManager em = entityManagerFactory.createEntityManager();
+		try {
+			em.getTransaction().begin();
+			if(compte.getNumero()==null) {
+			    em.persist(compte); //INSERT INTO avec auto_incrementation
+			}
+			else {
+			   em.merge(compte); //UPDATE
+			}
+			em.getTransaction().commit();
+		} catch (Exception e) {
+			em.getTransaction().rollback();
+			e.printStackTrace();
+		}
+		em.close();
+		return compte; //avec numero pas null (quelquefois auto_incrémenté)
+	}
+	*/
 
 	@Override
 	public List<Compte> findAll() {
