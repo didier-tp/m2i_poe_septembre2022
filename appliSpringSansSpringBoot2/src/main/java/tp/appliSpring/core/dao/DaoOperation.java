642bc2c1acdceb7a317f225ae0e5e4da706b2efd
DidierDefrance <didier@d-defrance.fr>
2022-11-08 16:03:40 +0100

    s

--- appliSpringSansSpringBoot2/src/main/java/tp/appliSpring/core/dao/DaoOperation.java
+++ appliSpringSansSpringBoot2/src/main/java/tp/appliSpring/core/dao/DaoOperation.java
@@ -7,6 +7,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import tp.appliSpring.core.entity.Operation;
 
 public interface DaoOperation extends JpaRepository<Operation,Long>{
-	
+	/*
+	//codé via @NamedQuery "Operation.findByAccountNumber"
 	List<Operation> findByAccountNumber(Long numCpt);
+	*/
+	
+	//codé via convention de nommage:
+	//...
 }
