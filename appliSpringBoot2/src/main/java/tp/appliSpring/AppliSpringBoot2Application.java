accefa3dddff03bbece374076e13d0abcc21bf0f
DidierDefrance <didier@d-defrance.fr>
2022-11-08 17:00:18 +0100

    debut springBoot

---
+++ appliSpringBoot2/src/main/java/tp/appliSpring/AppliSpringBoot2Application.java
@@ -0,0 +1,13 @@
+package tp.appliSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppliSpringBoot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppliSpringBoot2Application.class, args);
+	}
+
+}
---
+++ appliSpringBoot2/src/test/java/tp/appliSpring/AppliSpringBoot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package tp.appliSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppliSpringBoot2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
