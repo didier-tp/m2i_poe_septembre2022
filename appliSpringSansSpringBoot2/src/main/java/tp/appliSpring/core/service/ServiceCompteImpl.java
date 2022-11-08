a4a2957c9068c1f37fe9a7f7de6f6f1c5822f487
DidierDefrance <didier@d-defrance.fr>
2022-11-08 11:51:01 +0100

    s

--- appliSpringSansSpringBoot2/src/main/java/tp/appliSpring/core/service/ServiceCompteImpl.java
+++ appliSpringSansSpringBoot2/src/main/java/tp/appliSpring/core/service/ServiceCompteImpl.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import tp.appliSpring.core.dao.DaoCompte;
 import tp.appliSpring.core.entity.Compte;
+import tp.appliSpring.core.exception.SoldeInsuffisantException;
 
 //@Component
 @Transactional //souvent tout en haut de la classe sur un vrai projet d'entreprise
@@ -53,12 +53,18 @@ public class ServiceCompteImpl implements ServiceCompte {
 	//@Transactional(/* propagation = Propagation.REQUIRED par défaut */)
 	//maintenant @Transactional est placé dans le haut de la classe
 	public void transferer(double montant, long numCptDeb, long numCptCred) {
-		Compte cptDeb = daoCompte.findById(numCptDeb);
-		cptDeb.setSolde(cptDeb.getSolde() - montant);
-		daoCompte.save(cptDeb); //v1 ou v2 sans .save() si @Transactional
-		Compte cptCred = daoCompte.findById(numCptCred);
-		cptCred.setSolde(cptCred.getSolde() + montant);
-		daoCompte.save(cptCred); // v1  ou v2 sans .save() si @Transactional
+		try {
+			Compte cptDeb = daoCompte.findById(numCptDeb);
+			if(cptDeb.getSolde() < montant)
+				throw new SoldeInsuffisantException("solde insuffisant sur compte " + numCptDeb);
+			cptDeb.setSolde(cptDeb.getSolde() - montant);
+			daoCompte.save(cptDeb); //v1 ou v2 sans .save() si @Transactional
+			Compte cptCred = daoCompte.findById(numCptCred);
+			cptCred.setSolde(cptCred.getSolde() + montant);
+			daoCompte.save(cptCred); // v1  ou v2 sans .save() si @Transactional
+		} catch (Exception e) {
+			throw new RuntimeException("echec virement" , e);
+		}
 	}
 
 	@Override
