91afdac0b266761003f2508eaae0901d2dab2120
DidierDefrance <didier@d-defrance.fr>
2022-11-09 09:37:38 +0100

    index.html

--- appliSpringBoot2/src/main/java/tp/appliSpring/AppliSpringBoot2Application.java
+++ appliSpringBoot2/src/main/java/tp/appliSpring/AppliSpringBoot2Application.java
@@ -8,6 +8,7 @@ public class AppliSpringBoot2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppliSpringBoot2Application.class, args);
+		System.out.println("http://localhost:8080/appliSpringBoot");
 	}
 
 }
