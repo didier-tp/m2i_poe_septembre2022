5d89867d84c2337edeef8f4ed25aef92af6a7f31
DidierDefrance <didier@d-defrance.fr>
2022-11-14 16:50:32 +0100

    s

--- appliSpringBoot2/lancerAppliSpringBoot.bat
+++ appliSpringBoot2/lancerAppliSpringBoot.bat
@@ -2,6 +2,6 @@ cd /d "%~dp0/target"
 REM NB: in META-INF/MANIFEST.MF of .jar
 REM Main-Class: org.springframework.boot.loader.PropertiesLauncher
 REM Start-Class: tp.appliSpring.AppliSpringBootApplication
-java -Dspring.profiles.active=dev -jar appliSpringBoot-0.0.1-SNAPSHOT.jar
-REM java -Dspring.profiles.active=prod -jar appliSpringBoot-0.0.1-SNAPSHOT.jar
+java -Dspring.profiles.active=dev -jar appliSpringBoot2-0.0.1-SNAPSHOT.jar
+REM java -Dspring.profiles.active=prod -jar appliSpringBoot2-0.0.1-SNAPSHOT.jar
 pause
