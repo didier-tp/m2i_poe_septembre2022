f4447e95bfec11ee3f50abdb0fcf47dca4377a73
DidierDefrance <didier@d-defrance.fr>
2022-11-14 12:19:05 +0100

    s

--- appliSpringBoot2/src/test/java/tp/appliSpring/AppliSpringBoot2ApplicationTests.java
+++ appliSpringBoot2/src/test/java/tp/appliSpring/AppliSpringBoot2ApplicationTests.java
@@ -2,8 +2,10 @@ package tp.appliSpring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles({"dev"})
 class AppliSpringBoot2ApplicationTests {
 
 	@Test
