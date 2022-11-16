ff359c4d098782cca998d80b606ec3e48da72893
DidierDefrance <didier@d-defrance.fr>
2022-11-16 16:16:55 +0100

    s

--- appliSpringBoot2/src/main/resources/static/js/comptes.js
+++ appliSpringBoot2/src/main/resources/static/js/comptes.js
@@ -2,8 +2,17 @@ function searchComptes(){
 	let numClient = document.getElementById("txtNumClient").value;
 	let wsUrl = "./bank-api/compte?numClient="+numClient;
 	makeAjaxGetRequest(wsUrl , (jsonResponse) => {
-		let jsComptes = JSON.parse(jsonResponse);
-		//....
 		document.getElementById("spanRes").innerHTML=jsonResponse;
+		let jsComptes = JSON.parse(jsonResponse);
+		let tbodyElt = document.getElementById("tbody");
+		tbodyElt.innerHTML="";
+		for(let i in jsComptes){
+			let compte = jsComptes[i]; 
+			let trElt = tbodyElt.insertRow(-1);
+			trElt.insertCell(0).innerHTML=compte.numero;
+			trElt.insertCell(1).innerHTML=compte.label;
+			trElt.insertCell(2).innerHTML=compte.solde;
+		}
+		
 	});
 }
