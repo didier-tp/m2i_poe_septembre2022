ec224b7ff42ce1e743d11c3141dd40d6045da5f7
DidierDefrance <didier@d-defrance.fr>
2022-11-16 16:42:35 +0100

    debut tpAngular

---
+++ tpAngular/my-app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ComptesComponent } from './comptes/comptes.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ComptesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
