8bee12f471a069b4a538ce6546e4308144b0a2ce
DidierDefrance <didier@d-defrance.fr>
2022-11-16 16:55:11 +0100

    s

--- tpAngular/my-app/src/app/comptes/comptes.component.ts
+++ tpAngular/my-app/src/app/comptes/comptes.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComptesComponent implements OnInit {
 
+  numClient = 0;
+  listComptes :any[]= [];
+
+  searchComptes(){
+    this.listComptes.push({numero : 1 , label: "compteA" , solde : 50});
+    this.listComptes.push({numero : 2 , label: "compteB" , solde : 150});
+  }
+
   constructor() { }
 
   ngOnInit(): void {
